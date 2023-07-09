827ca92cf54bbd7b0e6f5d65218e2323f230e191
Sushmita <sushmitakumari363@gmail.com>
2023-07-09 20:14:10 +0530

    router added

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+
+export const Home = () => {
+  return (
+    <Box>
+      <Typography>Hello</Typography>
+    </Box>
+  );
+};
